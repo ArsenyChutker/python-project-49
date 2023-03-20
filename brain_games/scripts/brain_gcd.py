80d7412585b7f5b55ca109a925dde36a2ff15df4
Arseniy <arsenychutker@gmail.com>
2023-03-20 23:34:42 +0300

    --all

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,24 +1,25 @@
 #!/usr/bin/env python3
 import prompt
-from random import randint
 import random
+
 
 def main():
     print("Welcome to the Brain Games!")
     name = welcome_user()
-    print ('Find the greatest common divisor of given numbers.')
+    print('Find the greatest common divisor of given numbers.')
     win_count = 0
     while win_count < 3:
         win_count, our_answer, user_answer = play_round(win_count)
-        if(win_count == -1):
-            print (f""" '{user_answer}' is wrong answer ;(. Correct answer was '{our_answer}'.
+        if (win_count == -1):
+            print(f""" '{user_answer}' is wrong answer ;(. Correct answer was '{our_answer}'.
 Let's try again, {name}!""")
             break
-    
-    if(win_count == 3):
+
+    if (win_count == 3):
         print(f"""Congratulations, {name}!""")
     return
-    
+
+
 def welcome_user():
     name = prompt.string('May I have your name? ')
     print(f"Hello, {name}!")
@@ -26,22 +27,21 @@ def welcome_user():
 
 
 def play_round(win_count):
-    a = (random.randint(0,100))
-    b = (random.randint(0,100))
-    while a!=b:
+    a = (random.randint(0, 100))
+    b = (random.randint(0, 100))
+    while a != b:
         if a > b:
             a -= b
         else:
             b -= a
     our_answer = b
-    print (f'Question: {a} {b}')
+    print(f'Question: {a} {b}')
     user_answer = int(prompt.string("Your answer: "))
     if our_answer == user_answer:
-        print ('Correct!')
+        print('Correct!')
         return win_count + 1, our_answer, user_answer
     else:
         return -1, our_answer, user_answer
-
 
 
 if __name__ == '__main__':
--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,24 +1,25 @@
 #!/usr/bin/env python3
 import prompt
-from random import randint
 import random
+
 
 def main():
     print("Welcome to the Brain Games!")
     name = welcome_user()
-    print ('Answer "yes" if given number is prime, Otherwise answer "no".')
+    print('Answer "yes" if given number is prime, Otherwise answer "no".')
     win_count = 0
     while win_count < 3:
         win_count = play_round(win_count)
-        if(win_count == -1):
-            print (f"""'yes' is wrong answer ;(. Correct answer was 'no'.
+        if (win_count == -1):
+            print(f"""'yes' is wrong answer ;(. Correct answer was 'no'.
 Let's try again, {name}!""")
             break
-    
-    if(win_count == 3):
+
+    if (win_count == 3):
         print(f"""Congratulations, {name}!""")
     return
-    
+
+
 def welcome_user():
     name = prompt.string('May I have your name? ')
     print(f"Hello, {name}!")
@@ -26,15 +27,16 @@ def welcome_user():
 
 
 def play_round(win_count):
-    random_number = (random.randint(0,100))
-    print (f"Question: {random_number}")
+    random_number = (random.randint(0, 100))
+    print(f"Question: {random_number}")
     user_answer = prompt.string("Your answer: ")
-    our_answer =  Prime_or_not(random_number)
+    our_answer = Prime_or_not(random_number)
     if our_answer == user_answer:
-        print ('Correct!')
+        print('Correct!')
         return win_count + 1
     else:
         return -1
+
 
 def checkPrime(num):
     if num < 2:
@@ -43,7 +45,7 @@ def checkPrime(num):
         if num % i == 0:
             return False
     return True
-    
+
 
 def Prime_or_not(num):
     if checkPrime(num):
@@ -51,6 +53,6 @@ def Prime_or_not(num):
     else:
         return "no"
 
+
 if __name__ == '__main__':
     main()
-
